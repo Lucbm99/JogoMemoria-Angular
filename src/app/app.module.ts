cd6798edac1d8dcb5ea842decc1d687dc49bd699
Lucbm99 <lbaradelmarchiori@gmail.com>
2022-08-17 22:06:15 -0300

    Configurações iniciais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FlipCardModule } from './flip-card/flip-card.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlipCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
