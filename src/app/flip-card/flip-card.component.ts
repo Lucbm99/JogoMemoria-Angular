736b3de61d7d453d7f675bb4f65779bb214c8409
Lucbm99 <lbaradelmarchiori@gmail.com>
2022-08-23 21:52:59 -0300

    Fix - adição imagens cards e controle dos mesmos

--- src/app/flip-card/flip-card.component.ts
+++ src/app/flip-card/flip-card.component.ts
@@ -7,15 +7,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FlipCardComponent implements OnInit {
 
-  toggleProperty = false;
-  quantity = 15;
+  toggleProperty1 = false;
+  toggleProperty2 = false;
+  toggleProperty3 = false;
+  toggleProperty4 = false;
+  toggleProperty5 = false;
+  toggleProperty6 = false;
+  toggleProperty7 = false;
+  toggleProperty8 = false;
+  toggleProperty9 = false;
+  toggleProperty10 = false;
+  toggleProperty11 = false;
+  toggleProperty12 = false;
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  toggle() {
-    this.toggleProperty = !this.toggleProperty;
+  toggle1() {
+    this.toggleProperty1 = !this.toggleProperty1;
   }
+
+  toggle2() {
+    this.toggleProperty2 = !this.toggleProperty2;
+  }
+
+  toggle3() {
+    this.toggleProperty3 = !this.toggleProperty3;
+  }
+
+  toggle4() {
+    this.toggleProperty4 = !this.toggleProperty4;
+  }
+
+  toggle5() {
+    this.toggleProperty5 = !this.toggleProperty5;
+  }
+
+  toggle6() {
+    this.toggleProperty6 = !this.toggleProperty6;    
+  }
+
+  toggle7() {
+    this.toggleProperty7 = !this.toggleProperty7;
+  }
+
+  toggle8() {
+    this.toggleProperty8 = !this.toggleProperty8;
+  }
+
+  toggle9() {
+    this.toggleProperty9 = !this.toggleProperty9;
+  }
+
+  toggle10() {
+    this.toggleProperty10 = !this.toggleProperty10;
+  }
+  
+  toggle11() {
+    this.toggleProperty11 = !this.toggleProperty11;
+  }
+
+  toggle12() {
+    this.toggleProperty12 = !this.toggleProperty12;
+  }
+  
 }
