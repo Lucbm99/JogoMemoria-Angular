78d57a550ab4e5de4a37fe472ad213ace61eb2b4
Lucbm99 <lbaradelmarchiori@gmail.com>
2022-09-07 20:39:14 -0300

    Fix - pegando src da imagem

--- src/app/flip-card/flip-card.component.ts
+++ src/app/flip-card/flip-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FlipCardService } from './flip-card.service';
 
 @Component({
@@ -7,9 +7,18 @@ import { FlipCardService } from './flip-card.service';
   styleUrls: ['./flip-card.component.scss']
 })
 export class FlipCardComponent implements OnInit {
+  @ViewChild('dart', { static: true }) dart !: ElementRef;
+  @ViewChild('sass', { static: true }) sass !: ElementRef;
+  @ViewChild('docker', { static: true }) docker !: ElementRef;
+  @ViewChild('github', { static: true }) github !: ElementRef;
+  @ViewChild('golang', { static: true }) golang !: ElementRef;
+  @ViewChild('kotlin', { static: true }) kotlin !: ElementRef;
 
   public listaImagens: any;
   public listaImagensById: any;
+
+  public primeiroClique: any = []
+  public segundoClique: any = []
 
   toggleProperty1 = true;
   toggleProperty2 = true;
@@ -50,51 +59,67 @@ export class FlipCardComponent implements OnInit {
     );
   }
 
-  toggle1() {
+  toggle1(event: Event) {
+    const imgSass = this.sass.nativeElement.src;
+    if(event) {
+      this.primeiroClique.push(imgSass);
+    }
+    console.log(this.primeiroClique)
     this.toggleProperty1 = !this.toggleProperty1;
   }
 
-  toggle2() {
+  toggle2(event: Event) {
+    const imgDart = this.dart.nativeElement.src;
     this.toggleProperty2 = !this.toggleProperty2;
   }
 
   toggle3() {
+    const imgDocker = this.docker.nativeElement.src;
     this.toggleProperty3 = !this.toggleProperty3;
   }
 
   toggle4() {
+    const imgGithub = this.github.nativeElement.src;
     this.toggleProperty4 = !this.toggleProperty4;
   }
 
   toggle5() {
+    const imgGoLang = this.golang.nativeElement.src;
     this.toggleProperty5 = !this.toggleProperty5;
   }
 
   toggle6() {
+    const imgKotlin = this.kotlin.nativeElement.src;
     this.toggleProperty6 = !this.toggleProperty6;    
   }
 
   toggle7() {
+    const imgGithub = this.github.nativeElement.src;
     this.toggleProperty7 = !this.toggleProperty7;
   }
 
   toggle8() {
+    const imgDocker = this.docker.nativeElement.src;
     this.toggleProperty8 = !this.toggleProperty8;
   }
 
   toggle9() {
+    const imgSass = this.sass.nativeElement.src;
     this.toggleProperty9 = !this.toggleProperty9;
   }
 
   toggle10() {
+    const imgKotlin = this.kotlin.nativeElement.src;
     this.toggleProperty10 = !this.toggleProperty10;
   }
   
   toggle11() {
+    const imgDart = this.dart.nativeElement.src;
     this.toggleProperty11 = !this.toggleProperty11;
   }
 
   toggle12() {
+    const imgGoLang = this.golang.nativeElement.src;
     this.toggleProperty12 = !this.toggleProperty12;
   }
   
